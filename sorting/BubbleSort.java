19c043d92c6c913f28f8524b14afad1318efd8ee
Philip La <CreaturePhil@users.noreply.github.com>
2015-01-24 17:47:05 -0800

    Update BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort {
 	 * Bubble Sort repeatedly steps through the list to be sorted, compares each
 	 * pair of adjacent items and swaps them if they are in the wrong order. 
 	 * The pass through the list is repeated until no swaps are needed, which 
-	 * indicates that the list is sorted. Bubble Sort is a comparison sort, is 
+	 * indicates that the list is sorted. Bubble Sort is a comparison sort and is 
 	 * named for the way smaller elements "bubble" to the top of the list. 
 	 * Bubble Sort has O(n^2) time complexity.
 	 */
